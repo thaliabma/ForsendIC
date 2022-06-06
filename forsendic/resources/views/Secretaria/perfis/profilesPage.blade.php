b82fe8a5a751a422487eecf44546818847b2dab1
Hélder Silva Ferreira Lima <74564010+heldaolima@users.noreply.github.com>
2022-06-05 21:08:14 -0300

    removendo algumas sobras

--- forsendic/resources/views/Secretaria/perfis/profilesPage.blade.php
+++ forsendic/resources/views/Secretaria/perfis/profilesPage.blade.php
@@ -70,7 +70,6 @@
 
     <footer><strong>Todos os direitos reservados</strong></footer>
 
-    {{-- <script src="{{asset('css/app.js')}}"></script> --}}
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
