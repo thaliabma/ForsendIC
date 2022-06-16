de428ac12a577aada2fd1dd904718b1108e0df48
Murilo RIbeiro <murilourquiza@outlook.com>
2022-06-16 20:01:59 -0300

    Email senha e redefinicao

--- forsendic/app/Http/Controllers/AlunoController.php
+++ forsendic/app/Http/Controllers/AlunoController.php
@@ -39,15 +39,14 @@ class AlunoController extends Controller
         $formFields = $request->validate([
             'email' => ['required', 'email', new OnlyICValidation],
         ]);
-
-        // $password = rand(100000, 999999); //otp
         
         $existente = User::where('email', $formFields['email'])->first();
         if ($existente) {
             return $this->show_otp_form($existente['email']);
         }
 
-        $password = '123456';
+        $password = rand(100000, 999999); //otp
+        //$password = '123456';
         User::create([
             'name' => 'Aluno',
             'email' => $formFields['email'],
@@ -55,14 +54,14 @@ class AlunoController extends Controller
             'password' => Hash::make($password)
         ]);
 
-        // if (Mail::to($formFields['email'])->send(new SendOtp($password))) {
+         if (Mail::to($formFields['email'])->send(new SendOtp($password))) {
             return $this->show_otp_form($formFields['email']);
-        // }
-        // else {
-        //     return redirect()->back()->with([
-        //         'status' => 'Não conseguimos enviar a mensagem'
-        //     ]);
-        // }
+         }
+         else {
+             return redirect()->back()->with([
+                 'status' => 'Não conseguimos enviar a mensagem'
+             ]);
+         }
     }
 
     function checkAluno(Request $request){
--- forsendic/resources/views/Secretaria/redefinirSenhaEmail.blade.php
+++ forsendic/resources/views/Secretaria/redefinirSenhaEmail.blade.php
@@ -5,11 +5,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Redefinir senha da secretaria</title>
+
+    <style type="text/css">
+        body {
+            background-color: GhostWhite;
+        }
+
+        a:link, a:visited, a:active{
+	        text-decoration: none;
+        }
+
+        #imagem {
+	        position: absolute;
+	        z-index: 2;
+        }
+
+        #forsend {
+            width: 14%;
+            height: 9%;
+        }
+
+        #ufal {
+            width: 8%;
+            height: 13%;
+            float: right;
+        }
+
+        #tela {
+            opacity: none;
+	        display: flex;
+	        text-align: center;
+	        flex-direction: column;
+	        flex-wrap: nowrap;
+	        justify-content: space-around;
+	        position: absolute;
+	        z-index: 1;
+            width: 96%;
+            margin: 0 auto;
+        }
+
+    </style>
+
 </head>
 <body>
-    <h1>Redefinir senha </h1>
-    <a href="{{$url}}">Clique aqui para redefinir a sua senha</a>
-    
-    <p><em>Esse link funcionará por {{$count}} minutos </em></p>
+
+    <div id="imagem">
+        <img id="forsend" src="{{asset('/images/Logo_ForsendIC.png')}}">	
+        <img id="ufal" src="{{asset('/images/ufal.png')}}">
+	</div>
+
+    <div id="tela">
+        <h2 style="color:black">Redefinir senha</h2>
+        <p style="color:grey31">Caso deseje alterar a senha de acesso por parte da secretária: </p><br>
+        <a href="{{$url}}" style="color:Navy"><b>CLIQUE AQUI</b></a><br>
+        <p style="color:grey31">Esse link funcionará por {{$count}} minutos.</p>
+        <p style="color:grey31">Atenciosamente, <br>Equipe do ForsendIC</p> 
+    </div>
 </body>
 </html>
