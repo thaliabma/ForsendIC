f669d3e53958458618789c2ce57319749b2f3328
thaliabma <tbma@ic.ufal.br>
2022-06-28 22:04:42 -0300

    Melhorando o menu de formulários

--- forsendic/resources/views/alunos/MenuFormularios.blade.php
+++ forsendic/resources/views/alunos/MenuFormularios.blade.php
@@ -2,34 +2,36 @@
 	<head>
 		<title>Estilo CSS externo</title>
 		<meta charset="utf-8">
-
-		<link rel="stylesheet" type="text/css" href="{{asset('css/alunos/estilo2.css')}}">
+        <meta
+            name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+		<link rel="stylesheet" type="text/css" href="{{asset('css/alunos/style_Menu.css')}}">
 
 	</head>
 
 	<body>
-		<x-flash-message />
 		<div id="cabecalho">
-			<img id="forsend" src="{{asset('/images/Logo_ForsendIC.png')}}">	
-
+			<img id="forsend" src="{{asset('/images/Logo_ForsendIC.png')}} " >
+            <div id="grow"></div>
 			<img id="ufal" src="{{asset('/images/ufal.png')}}">
 		</div>
 
 		<div id="caixa">
 			<h1 id="titulo">FORMULÁRIOS</h1>
-			@if (isset($message))
-			<h2>{{$message}}</h2>
-			@endif
+
 			<p id="subtitulo">Selecione abaixo o formulário que deseja enviar para secretaria:</p>
 
-			<a class="forms" href="{{route('aluno.desistencia')}}">Desistência de Vínculo Total de Curso</a>
-			<a class="forms" href="{{route('aluno.trancamento')}}">Trancamento de Matrícula de Disciplina</a>
-			<a class="forms" href="{{route('aluno.rematricula')}}">Rematrícula</a>
+            <a class="forms btn btn-primary" href="{{route('aluno.desistencia')}}" role="button">Desistência de Vínculo Total de Curso</a>
+            <a class="forms btn btn-primary" href="{{route('aluno.trancamento')}}" role="button">Trancamento de Matrícula de Disciplina</a>
+            <a class="forms btn btn-primary" href="{{route('aluno.rematricula')}}" role="button">Rematrícula</a>
 			<form method="post" action="{{route('aluno.logout')}}">
 				@csrf
-				<button id="sair" type="submit">⍈ Sair</a> 			
+				<button id="sair" type="submit">Sair</a>
 			</form>
-		</div> 
+		</div>
 
 	</body>
 
