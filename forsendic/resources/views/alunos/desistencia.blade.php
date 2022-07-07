0ca04d9f94bff44c5829f0e6f9ada06106c81038
PedroMesquitaIsidoro <phmi@ic.ufal.br>
2022-07-07 12:00:58 -0300

    Atualização com Arrastar ou Clicar pra enviar pdf

--- forsendic/resources/views/alunos/desistencia.blade.php
+++ forsendic/resources/views/alunos/desistencia.blade.php
@@ -56,7 +56,19 @@
                         <label class="labelCampos">Anexar (somente em pdf) :</label><br>
 
                         {{-- <label class="labelFile" for="arquivo1" id="arq1"> ⇩ Cópia do RG ou CNH</label> --}}
-                        <input type="file" name="file" id="arquivo1"><br>
+
+						<!-- ÁREA DO "CLIQUE OU ARRASTE AQUI -->
+							<div class="area-upload">
+								<label for="arquivo1" class="label-upload">
+									<i class="fas fa-cloud-upload-alt"></i>
+							<div class="texto">Clique ou arraste o arquivo</div>
+							</label>
+								<input type="file" name="file" id="arquivo1" multiple/>
+						
+								<div class="lista-uploads">
+								</div>
+							</div>
+						<!-- FIM DA ÁREA DO "CLIQUE OU ARRASTE AQUI -->
                         @error('file')
                             {{$message}}
                         @enderror
@@ -67,6 +79,102 @@
                         <input type="file" name="arquivo3" id="arquivo3"><br> --}}
                     </div>
                 </div>
+				<script>
+						let drop_ = document.querySelector('.area-upload #arquivo1');
+						drop_.addEventListener('dragenter', function(){
+							document.querySelector('.area-upload .label-upload').classList.add('highlight');
+						});
+						drop_.addEventListener('dragleave', function(){
+							document.querySelector('.area-upload .label-upload').classList.remove('highlight');
+						});
+						drop_.addEventListener('drop', function(){
+							document.querySelector('.area-upload .label-upload').classList.remove('highlight');
+						});
+
+						document.querySelector('#arquivo1').addEventListener('change', function() {
+						var files = this.files;
+						for(var i = 0; i < files.length; i++){
+							var info = validarArquivo(files[i]);
+							
+							//Criar barra
+							var barra = document.createElement("div");
+							var fill = document.createElement("div");
+							var text = document.createElement("div");
+							barra.appendChild(fill);
+							barra.appendChild(text);
+							
+							barra.classList.add("barra");
+							fill.classList.add("fill");
+							text.classList.add("text");
+							
+							if(info.error == undefined){
+								text.innerHTML = info.success;
+								enviarArquivo(i, barra); //Enviar
+							}else{
+								text.innerHTML = info.error;
+								barra.classList.add("error");
+							}
+							
+							//Adicionar barra
+							document.querySelector('.lista-uploads').appendChild(barra);
+						};
+					});
+
+					function validarArquivo(file){
+					console.log(file);
+					// Tipos permitidos
+					var mime_types = [ 'application/pdf'];
+					
+					// Validar os tipos
+					if(mime_types.indexOf(file.type) == -1) {
+						return {"error" : "O arquivo " + file.name + " não permitido"};
+					}
+
+					// Apenas 700MB é permitido
+					if(file.size > 700*1024*1024) {
+						return {"error" : file.name + " ultrapassou limite de 700MB"};
+					}
+
+					// Se der tudo certo
+					return {"success": "Enviando: " + file.name};
+				}
+
+					function enviarArquivo(indice, barra){
+					var data = new FormData();
+					var request = new XMLHttpRequest();
+					
+					//Adicionar arquivo
+					data.append('file', document.querySelector('#arquivo1').files[indice]);
+					
+					// AJAX request finished
+					request.addEventListener('load', function(e) {
+						// Resposta
+						if(request.response.status == "success"){
+							barra.querySelector(".text").innerHTML = "<a href=\"" + request.response.path + "\" target=\"_blank\">" + request.response.name + "</a> <i class=\"fas fa-check\"></i>";
+							barra.classList.add("complete");
+						}else{
+							barra.querySelector(".text").innerHTML = "Erro ao enviar: " + request.response.name;
+							barra.classList.add("error");
+						}
+					});
+					
+					// Calcular e mostrar o progresso
+					request.upload.addEventListener('progress', function(e) {
+						var percent_complete = (e.loaded / e.total)*100;
+						
+						barra.querySelector(".fill").style.minWidth = percent_complete + "%"; 
+					});
+
+					//Resposta em JSON
+					request.responseType = 'json';
+					
+					// Caminho
+					request.open('post', 'upload.php'); 
+					request.send(data);
+				}
+
+
+					</script>
                 <div class="botoes">
                     <a  class="butao1 btn d-inline" style="background: #355592; color: #fff;" href="{{route('aluno.forms')}}" role="button">Voltar</a>
                     <input class="butao2 btn d-inline" style="background: #355592; color: #fff;" id="enviar" type="submit" value="Enviar">
